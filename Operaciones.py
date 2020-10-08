d03eb499194159e678e27b0d399f286ec4148354
sza95 <sza95@outlook.com>
2020-10-08 18:49:37 -0500

    Added basic math operations file

---
+++ Operaciones.py
@@ -0,0 +1,33 @@
+
+
+print("Ingrese el valor para A")
+A = int(input())
+
+print("Ingrese el valor para B")
+B = int(input())
+
+
+
+print("")
+print(f"El valor de A es: {A} y el valor de B es: {B}")
+print("")
+
+print("Suma")
+C = A + B 
+print(C)
+print("")
+
+print("Resta")
+C = A - B
+print(C)
+print("")
+
+print("Multiplicación")
+C = A * B
+print(C)
+print("")
+
+print("División")
+C = A / B
+print(C)
+print("")
